1a0ac5e26e97b84ddc8159fe6f54b56c6041b889
salvath basha shaik <salvathshaik786143@gmail.com>
2018-11-02 10:30:12 -0700

    [ADD] upto otp ,and basic UI

---
+++ app/src/main/java/com/mypushtak/app/CartItemsActivity.java
@@ -0,0 +1,13 @@
+package com.mypushtak.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_items);
+    }
+}
